decbe74c6e488bc60ac7ca93286441ff7fbed8e4
Alexander Khardin <khporter1984@gmail.com>
2022-06-12 11:13:00 +0300

    task-02 is complete

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,12 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const ingredientsEl = document.querySelector('#ingredients');
+const ingredientsItem = [];
+
+for (const idx in ingredients) {
+  ingredientsItem[idx] = document.createElement('li');
+  ingredientsItem[idx].append(document.createTextNode(ingredients[idx]));
+  ingredientsItem[idx].classList.add('item');
+}
+
+ingredientsEl.append(...ingredientsItem);
