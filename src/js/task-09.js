cbf875b3b8228640e1d03a3d50460685e3dd938c
Alexander Khardin <khporter1984@gmail.com>
2022-06-15 20:33:57 +0300

    task-09 is complete

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBtn = document.querySelector('.change-color');
+const bodyEl = document.querySelector('body');
+const colorValueEl = document.querySelector('.color');
+
+changeColorBtn.addEventListener('click', event => {
+  const color = getRandomHexColor();
+  bodyEl.style.backgroundColor = color;
+  colorValueEl.textContent = color;
+});
