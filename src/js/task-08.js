b2da8e617ec45bbb1bf74c4591a9a5c599d6ba31
Alexander Khardin <khporter1984@gmail.com>
2022-06-15 14:27:54 +0300

    task-08 is complete

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,21 @@
+const loginFormEl = document.querySelector('.login-form');
 
+loginFormEl.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const email = event.currentTarget.elements.email.value;
+  const password = event.currentTarget.elements.password.value;
+
+  if (email && password) {
+    const formData = {
+      email,
+      password,
+    };
+
+    console.log(formData);
+
+    event.currentTarget.reset();
+  } else {
+    alert('Все поля должны быть заполнены!!!');
+  }
+});
