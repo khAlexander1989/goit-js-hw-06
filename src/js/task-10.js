a34c88525ae8b6af0210a2fcc0191810781d1e05
Alexander Khardin <khporter1984@gmail.com>
2022-06-16 00:31:57 +0300

    task-10 is complete

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,38 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const boxesEl = document.querySelector('#boxes');
+const createBoxesBtn = document.querySelector('[data-create]');
+const destroyBoxesBtn = document.querySelector('[data-destroy]');
+const boxesAmountField = document.querySelector('#controls input');
+
+function createBoxes(amount) {
+  const boxes = [];
+  let boxSize = 30;
+
+  for (let i = 0; i < amount; i++) {
+    boxes.push(document.createElement('div'));
+
+    boxes[i].style.width = `${boxSize}px`;
+    boxes[i].style.height = `${boxSize}px`;
+    boxes[i].style.backgroundColor = getRandomHexColor();
+
+    boxSize += 10;
+  }
+
+  boxesEl.append(...boxes);
+}
+
+function destroyBoxes() {
+  while (boxesEl.firstChild) {
+    boxesEl.removeChild(boxesEl.firstChild);
+  }
+}
+
+createBoxesBtn.addEventListener('click', () => {
+  createBoxes(boxesAmountField.value);
+});
+destroyBoxesBtn.addEventListener('click', () => {
+  destroyBoxes();
+});
